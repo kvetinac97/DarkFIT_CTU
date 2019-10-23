ba70418ef0a3a307b0ae93452c2e274464f16363
kvetinac97 <kvetinac97@email.cz>
2019-10-23 07:28:40 +0200

    Initial Commit

---
+++ DarkFIT/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DarkFIT
+//
+//  Created by Thats Me on 23/10/2019.
+//  Copyright © 2019 Ondrej Wrzecionko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ DarkFIT/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DarkFIT
+//
+//  Created by Thats Me on 23/10/2019.
+//  Copyright © 2019 Ondrej Wrzecionko. All rights reserved.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = "DarkFIT";
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "cz.kvetinac97.DarkFIT-Extension") { error in
+            if let _ = error {
+                // Insert code to inform the user that something went wrong.
+
+            }
+        }
+    }
+
+}
